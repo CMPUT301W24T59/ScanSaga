01fe7be35a5fc669d95cc6bdf52971c3925672cc
Parinay-kapoor <144638387+Parinay-kapoor@users.noreply.github.com>
2024-02-28 19:27:58 -0700

    Backup thing

---
+++ ScanSaga/ScanSaga/ScanSaga/app/src/main/java/my/application/scansaga/ChooseOne.java
@@ -0,0 +1,14 @@
+package my.application.scansaga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_one);
+    }
+}
