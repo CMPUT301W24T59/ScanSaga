47d0ecc4867e1a7b6c583d776454e41300dc7070
Parinay-kapoor <144638387+Parinay-kapoor@users.noreply.github.com>
2024-03-06 18:55:24 -0700

    Merge branch 'rosybudhathoki' into eventPoster
    # Conflicts:
    #	.idea/deploymentTargetDropDown.xml
    #	app/build.gradle.kts

--- app/src/androidTest/java/com/example/scansaga/MainActivityTest.java
+++ app/src/androidTest/java/com/example/scansaga/MainActivityTest.java
@@ -9,7 +9,6 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import static org.mockito.AdditionalMatchers.not;
 
 import android.view.View;
 
