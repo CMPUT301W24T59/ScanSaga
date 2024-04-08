e99ad900722e72591d97dc70c0b2c4917f827dd5
Kaylem Nice <knice@ualberta.ca>
2024-04-08 10:25:37 -0600

    scan and go fixed for attendee

--- app/src/main/java/com/example/scansaga/Views/AddEventFragment.java
+++ app/src/main/java/com/example/scansaga/Views/AddEventFragment.java
@@ -296,7 +296,7 @@ public class AddEventFragment extends DialogFragment {
             return;
         }
 
-        String qrContent = eventName + "\n" + date + "\n" + venue;
+        String qrContent = eventName + "_" + date;
         Bitmap qrBitmap = generateQRCode(qrContent);
 
             // Upload QR code image to Firebase Storage
